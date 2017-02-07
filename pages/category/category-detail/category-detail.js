15ecfa36f674a313ed1d670983cec37448ebfdc2
Fangstyle <fangstyles@163.com>
2017-02-07 09:58:28 +0800

    解决音乐播放与微信后台音乐开关不统一问题

--- pages/category/category-detail/category-detail.js
+++ pages/category/category-detail/category-detail.js
@@ -1,75 +1,90 @@
 var postsData = require('../../../data/category-data.js');
 Page({
-    onLoad:function (option) {
+    onLoad: function (option) {
         var id = option.id;
+        var self = this;
         var data = postsData.postList[id];
-        var colletionKey = "detail"+id;
+        var colletionKey = "detail" + id;
         this.setData({
-            currentId:id,
-            dataList:data,
-            colletionKey:colletionKey
+            currentId: id,
+            dataList: data,
+            colletionKey: colletionKey
         });
 
         var detailLocalStorgeList = wx.getStorageSync('detail_collected_list_storage');
-        if (detailLocalStorgeList){
+        if (detailLocalStorgeList) {
             var isCollected = detailLocalStorgeList[id];
             this.setData({
                 collected: isCollected
             })
-        }else {
+        } else {
             var detailLocalStorgeList = {};
             detailLocalStorgeList[id] = false;
             wx.setStorageSync('detail_collected_list_storage', detailLocalStorgeList);
         }
+        /*监听音乐播放。*/
+        wx.onBackgroundAudioPlay(function () {
+            self.setData({
+                isPlayingMusic: true
+            })
+        });
+
+        /*监听音乐暂停。*/
+        wx.onBackgroundAudioPause(function () {
+            self.setData({
+                isPlayingMusic: false
+            })
+        });
+
     },
-    data:{
-        isPlayingMusic:false
+    data: {
+        isPlayingMusic: false
     },
-    onShareTap:function () {
-        var itemList = ['分享到微信好友', '分享到QQ好友', '分享到朋友圈','分享到QQ空间'];
+    onShareTap: function () {
+        var itemList = ['分享到微信好友', '分享到QQ好友', '分享到朋友圈', '分享到QQ空间'];
         wx.showActionSheet({
-            itemList:itemList,
-            success: function(res) {
+            itemList: itemList,
+            success: function (res) {
                 wx.showToast({
-                    title: '成功'+itemList[res.tapIndex],
+                    title: '成功' + itemList[res.tapIndex],
                     icon: 'success',
                     duration: 2000
                 })
             },
-            fail: function(res) {
+            fail: function (res) {
                 wx.showToast({
-                    title: '失敗'+res.errMsg,
+                    title: '失敗' + res.errMsg,
                     icon: 'success',
                     duration: 2000
                 })
             }
         })
     },
-    onColletionTap:function () {
+    onColletionTap: function () {
         var index = this.data.currentId;
         var detailLocalStorgeList = wx.getStorageSync('detail_collected_list_storage');
-        var  isCollected = detailLocalStorgeList[index];
+        var isCollected = detailLocalStorgeList[index];
         detailLocalStorgeList[index] = !isCollected;
         this.setData({
             collected: !isCollected
         })
         wx.setStorageSync('detail_collected_list_storage', detailLocalStorgeList);
     },
-    onMediaPlay:function(){
+    onMediaPlay: function () {
         var index = this.data.currentId;
         var musicJson = postsData.postList[index].music;
         var isPlayingMusic = !(this.data.isPlayingMusic);
-        if(isPlayingMusic){
+        if (isPlayingMusic) {
             wx.playBackgroundAudio({
                 dataUrl: musicJson.url,
-                title:  musicJson.title,
+                title: musicJson.title,
                 coverImgUrl: musicJson.coverImg
             })
-        }else{
+        } else {
             wx.pauseBackgroundAudio();
         }
         this.setData({
-            isPlayingMusic:isPlayingMusic
+            isPlayingMusic: isPlayingMusic
         })
     }
 });
